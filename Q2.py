9290b826490f93ee66a124b88299b24f2dacd34c
Geshan Anupama <129172291+Geshananupama@users.noreply.github.com>
2025-05-28 18:39:49 +0530

    first commit

--- Q2.py
+++ Q2.py
@@ -3,4 +3,6 @@ def Findmaxumnum(no1,no2):
         print("max.number is =",no2)
     else:
         print("max.number is =",no1)
-Findmaxumnum(3,2)
+no1=int(input("enterno1:"))
+no2=int(input("enterno1:"))
+Findmaxumnum(no1,no2)
